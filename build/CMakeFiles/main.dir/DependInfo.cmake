1208888f55b8636d94fe12ca62874e95e9d3a3f8
Fischer <roxane.fischer@polytechnique.edu>
2019-11-17 20:26:58 +0100

    Add structure

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/main.cpp" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/CMakeFiles/main.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glad/include"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/include"
+  "/usr/local/Cellar/opencv/4.1.1_2/include/opencv4"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/cmake/../include"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/cmake/../external/eigen"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.14.sdk/System/Library/Frameworks/OpenGL.framework"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/cocoa_init.m" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/cocoa_init.m.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/cocoa_joystick.m" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/cocoa_monitor.m" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/cocoa_time.c" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/cocoa_time.c.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/cocoa_window.m" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/cocoa_window.m.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/context.c" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/context.c.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/egl_context.c" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/egl_context.c.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/init.c" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/init.c.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/input.c" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/input.c.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/monitor.c" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/monitor.c.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/nsgl_context.m" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/nsgl_context.m.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/osmesa_context.c" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/posix_thread.c" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/posix_thread.c.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/vulkan.c" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src/window.c" "/Users/roxanefischer/INF574/projet_vectorization/code_Image_Vectorization/build/glfw/src/CMakeFiles/glfw.dir/window.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_GLFW_USE_CONFIG_H"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/opencv/4.1.1_2/include/opencv4"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/include"
+  "/Users/roxanefischer/INF574/projet_vectorization/libigl/external/glfw/src"
+  "glfw/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
